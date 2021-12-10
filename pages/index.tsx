f1ffb9b35926429555611ff77b1d93a7a05dc1c7
Nuri Hodges <nuri@irunbackwards.com>
2021-12-10 15:52:16 -0800

    chore: workflow fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import Header from '../lib/components/Header'
@@ -167,10 +166,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Powered by Base64
         </a>
       </footer>
     </>
