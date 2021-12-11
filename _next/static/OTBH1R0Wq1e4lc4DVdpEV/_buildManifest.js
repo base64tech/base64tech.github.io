3b8e2fc13fad5a7c7fb58b023f608a1008be9ba0
nhodges <nhodges@users.noreply.github.com>
2021-12-11 07:39:38 +0000

    deploy: 0aa62839e05d3d8284f51396ae7909716d31aa16

---
+++ _next/static/OTBH1R0Wq1e4lc4DVdpEV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fdaa8e5315d47635.css","static/chunks/pages/index-c73fb233b81edfc7.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/UO6UTTHkBQyFplfzjt_xv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fdaa8e5315d47635.css","static/chunks/pages/index-0fe2e26e2c8403ce.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
